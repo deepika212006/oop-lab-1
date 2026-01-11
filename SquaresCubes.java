c8a36b984c3fb833f6c08a3ca4436556f78e9e62
deepika212006 <dnc212006@gmail.com>
2026-01-11 16:46:52 +0530

    Add files via upload

--- SquaresCubes.java
+++ SquaresCubes.java
@@ -1,13 +1,39 @@
 // Q6)Write an application that calculate the square and cubes of number from 0to10 and print the resulting value 
 // in table format. 
 
-import java.util.Scanner;
+//without classes and objects
+
+// import java.util.Scanner;
+
+// public class SquaresCubes {
+//     public static void main(String[] args) {
+//         System.out.printf("Number\tSquare\tCube\n");
+//         for (int i = 0; i <= 10; i++) {
+//             System.out.printf("%d\t%d\t%d\n", i, (int) Math.pow(i, 2), (int) Math.pow(i, 3));
+//         }
+//     }
+// }
+
+// with classes and objects
+class Square {
+    public double square(int n) {
+        return Math.pow(n, 2);
+    }
+}
+
+class Cube {
+    public double cube(int n) {
+        return Math.pow(n, 3);
+    }
+}
 
 public class SquaresCubes {
     public static void main(String[] args) {
-        System.out.printf("Number\tSquare\tCube\n");
+        System.out.println("Number\tSquare\tCube");
+        Square s = new Square();
+        Cube c = new Cube();
         for (int i = 0; i <= 10; i++) {
-            System.out.printf("%d\t%d\t%d\n", i, (int) Math.pow(i, 2), (int) Math.pow(i, 3));
+            System.out.println(i + "\t" + (int) s.square(i) + "\t" + (int) c.cube(i));
         }
     }
 }
