04ee405ab2b0a69ecf664e83a863bc1ec01e1bfa
deepika212006 <dnc212006@gmail.com>
2026-01-11 17:00:56 +0530

    Add files via upload

--- ThreeNum.java
+++ ThreeNum.java
@@ -3,6 +3,38 @@
 
 import java.util.Scanner;
 
+class Sum {
+    Sum(int a, int b, int c) {
+        System.out.println("Sum is:" + (a + b + c));
+    }
+}
+
+class Average {
+    Average(int a, int b, int c) {
+        System.out.println("Average is:" + (a + b + c) / 3.0);
+    }
+}
+
+class Product {
+    Product(int a, int b, int c) {
+        System.out.println("Product is:" + (a * b * c));
+    }
+}
+
+class Largest {
+    Largest(int a, int b, int c) {
+        int largest = (a > b) ? (a > c ? a : c) : (b > c ? b : c);
+        System.out.println("Largest is:" + largest);
+    }
+}
+
+class Smallest {
+    Smallest(int a, int b, int c) {
+        int smallest = (a < b) ? (a < c ? a : c) : (b < c ? b : c);
+        System.out.println("Smallest is:" + smallest);
+    }
+}
+
 public class ThreeNum {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
@@ -10,17 +42,10 @@ public class ThreeNum {
         int a = input.nextInt();
         int b = input.nextInt();
         int c = input.nextInt();
-        int sum = a + b + c;
-        float average = sum / 3.0f;
-        int product = a * b * c;
-        int largest = (a > b) ? (a > c ? a : c) : (b > c ? b : c);
-        int smallest = (a < b) ? (a < c ? a : c) : (b < c ? b : c);
-        System.out.println("Sum is:" + sum);
-        System.out.println("Average is:" + average);
-        System.out.println("Product is:" + product);
-        System.out.println("Largest is:" + largest);
-        System.out.println("Smallest is:" + smallest);
-
+        Sum sumObj = new Sum(a, b, c);
+        Average avgObj = new Average(a, b, c);
+        Product prodObj = new Product(a, b, c);
+        Largest largeObj = new Largest(a, b, c);
+        Smallest smallObj = new Smallest(a, b, c);
     }
-
 }
